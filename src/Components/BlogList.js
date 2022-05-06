f59b55b4fb522d419ce4a21e4b9c9f01f302fbbe
miapiar <meri.lacouturiere@gmail.com>
2022-05-06 22:30:49 +0000

    fetching posts

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,18 +1,12 @@
-const BlogList = ({ blogs, onDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
+      <h2>{title}</h2>
       {blogs.map(blog => (
         <div key={blog.id} className="blog-preview">
           <h2>{blog.title}</h2>
           <p>{blog.body}</p>
           <p>Written by {blog.author}</p>
-          <button
-            onClick={() => {
-              onDelete(blog.id);
-            }}
-          >
-            delete blog
-          </button>
         </div>
       ))}
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const deleteBlog = id => {
-    setBlogs(blogs.filter(blog => blog.id !== id));
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:9000/blogs')
+        .then(res => {
+          if (!res.ok) {
+            throw Error('Could not fetch data for that resource');
+          }
+
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, []);
+
+  // const deleteBlog = id => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} onDelete={deleteBlog} />
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 };
