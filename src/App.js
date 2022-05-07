61a55aa0c1ca380e31d86fb005b8ef73b7ee5cb1
miapiar <meri.lacouturiere@gmail.com>
2022-05-07 12:21:25 +0000

    404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Create from './Components/Create';
 import BlogDetails from './Components/BlogDetails';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
             <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
